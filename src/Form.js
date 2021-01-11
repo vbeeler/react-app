e4c7623bc244f89718ebc60d3cd642650a07d0db
Vanessa Beeler <vbeeler@calpoly.edu>
2021-01-11 11:05:10 -0800

    submit functionality

--- src/Form.js
+++ src/Form.js
@@ -16,6 +16,11 @@ class Form extends Component {
     })
   }
 
+  submitForm = () => {
+    this.props.handleSubmit(this.state)
+    this.setState(this.initialState)
+  }
+
   render() {
     const { name, job } = this.state;
   
@@ -35,6 +40,10 @@ class Form extends Component {
           id="job"
           value={job}
           onChange={this.handleChange} />
+        <input 
+          type="button" 
+          value="Submit"
+          onClick={this.submitForm} />
       </form>
     );
   }
