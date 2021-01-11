9a5a9e67469e228e6ed0674c233d397e08340334
Vanessa Beeler <vbeeler@calpoly.edu>
2021-01-11 10:36:49 -0800

    delete button to remove characters

--- src/Table.js
+++ src/Table.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  )
+}
+
+const TableBody = props => {
+  const rows = props.characterData.map((row, index) => {
     return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     )
-  }
+  })
 
-  const TableBody = props => {
-    const rows = props.characterData.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
-      )
-    })
-  
-    return <tbody>{rows}</tbody>
-  }
+  return <tbody>{rows}</tbody>
+}
 
-  class Table extends Component {
-    render() {
-        const { characterData } = this.props
-    
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData}/>
-        </table>
-      )
-    }
-  }
+const Table = props => {
+  const { characterData, removeCharacter } = props
 
-  export default Table
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
+}
+
+export default Table
